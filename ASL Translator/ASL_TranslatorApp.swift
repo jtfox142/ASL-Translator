a346776de7627d0bd80725c2505e4fd057b09c3a
Jacob (J.T.) Fox <jtfox142@gmail.com>
2024-03-13 14:52:51 -0500

    Initial Commit

---
+++ ASL Translator/ASL_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASL_TranslatorApp.swift
+//  ASL Translator
+//
+//  Created by Jacob Fox on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ASL_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
